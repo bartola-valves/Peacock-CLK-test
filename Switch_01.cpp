eb8720502d5d9c2d708f87d711b6b2d1bd3cb3c2
Ale Moglia <valves@bartola.co.uk>
2024-07-27 12:12:46 +0100

    Initial commit

---
+++ Switch_01.cpp
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include <button.h>
+// Map all the LEDs in the PCK PCB
+#define LED_PIN 25  //on board LED
+#define LED_SW1 8  // LED SW1 on GP8
+#define LED_SW2 9  // LED SW2 on GP9
+#define LED_OUT_7 7  // LED OUT7 on GP7
+#define LED_OUT_6 6  // LED OUT7 on GP6
+
+// Map all the Inputs
+
+#define CLK 12  // Clock input
+#define RES 13  // Reset input
+#define SYNC 14 // Sync input
+
+// Map the switches
+#define SW1 10  // Switch 1
+#define SW2 11  // Switch 2
+
+// Global variable declaration here
+
+bool latch1; // stores switch 1 latching state
+bool latch2; // stores switch 2 latching state
+
+uint read_input(uint input)
+{
+    return gpio_get(input);
+}
+void test_LED(uint LED, int time)
+{
+    // turn on and off the LED for time*2
+    sleep_ms(time);
+    gpio_put(LED, 1);
+    sleep_ms(time);
+    gpio_put(LED, 0);
+    
+}
+
+// init_port initialises the port (either LED or jack) with direction. No logic validation of HW, be careful!
+// direction is GPIO_OUT or GPIO_IN
+void init_port( uint LED, bool direction)
+{
+    gpio_init(LED); //initialise the LED
+    gpio_set_dir(LED, direction); // set the LED direction
+}
+
+void onchange(button_t *button_p) 
+{
+  button_t *button = (button_t*)button_p;
+  
+  printf("Button on pin %d changed its state to %d\n", button->pin, button->state);
+
+  if(button->state) 
+  {
+    gpio_put(LED_SW1, 0);
+    gpio_put(LED_SW2, 0);
+    // turn off the LED when released
+    return; // Ignore button release. Invert the logic if using
+            // a pullup (internal or external).
+
+  }
+  switch(button->pin){
+    case SW1:
+    {
+        printf("SW1\n");
+        gpio_put(LED_SW1, 1);
+        // latch state change
+        if (!latch1)
+            latch1=true; 
+        else latch1 = false;
+        printf("SW1 latch state is: %d\n", latch1);
+        break;
+    }
+    case SW2:
+    {
+        printf("SW2\n");
+        gpio_put(LED_SW2, 1);
+        break;
+    }
+  }
+}
+
+void onchange2(button_t *button_p) 
+{
+  button_t *button = (button_t*)button_p;
+  
+  printf("Button on pin %d changed its state to %d\n", button->pin, button->state);
+
+  if(button->state) 
+  {
+    gpio_put(LED_SW2, 0);
+    // turn off the LED when released
+    return; // Ignore button release. Invert the logic if using
+            // a pullup (internal or external).
+
+  }
+  switch(button->pin){    
+    case SW2:
+    {
+        printf("SW2\n");
+        gpio_put(LED_SW2, 1);
+        break;
+    }
+  }
+}
+
+
+void latch_LEDs(void)
+/* very simple testing function to turn on LED when latch is active after pressing button 1
+    It uses global variable latch1*/
+{
+    if (latch1)
+        gpio_put(LED_OUT_7, 1);
+    else 
+        gpio_put(LED_OUT_7, 0);
+}
+
+int main()
+{
+    stdio_init_all();
+
+    init_port(LED_SW1,GPIO_OUT);
+    init_port(LED_SW2,GPIO_OUT);
+    button_t *sw1_button = create_button(SW1, onchange);
+    button_t *sw2_button = create_button(SW2, onchange2);
+
+    init_port(LED_OUT_7, GPIO_OUT);
+
+    latch1 = false;
+    latch2 = false;
+
+    while (true) {
+        printf("Hello, world!\n");
+        latch_LEDs();
+        sleep_ms(1000);
+    }
+}
